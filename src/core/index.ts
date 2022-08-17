c4264597125e991fe7f361344af9b1f2032a518f
chen <1443977802@qq.com>
2022-08-18 00:22:36 +0800

    初始化tracking

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,18 @@
-export const a = 1;
+import {DefaultOptons,TrackerConfig,Options} from '../types/index'
 
-console.log(a)
+export default class Tracking {
+    public data:Options;
+    constructor (options: Options){
+        this.data = Object.assign(this.initDef(),options)
+    }
+
+    private initDef ():DefaultOptons {
+        return <DefaultOptons>{
+            sdkVersion:TrackerConfig.version,
+            historyTracker:false,
+            hashTracker: false,
+            domTracker:false,
+            jsError: false,
+        }
+    }
+}
